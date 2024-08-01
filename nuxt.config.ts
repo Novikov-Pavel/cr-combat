bae0b9004f467607df8ed6953448af12da187763
Pavel Novikov <novikov@jobfrontend.ru>
2024-08-01 19:12:26 +0300

    12

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,6 +17,11 @@ export default defineNuxtConfig({
           href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
         },
       ],
+      script: [
+        {
+          src: "https://telegram.org/js/telegram-web-app.js",
+        },
+      ],
     },
   },
 });
--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,12 @@
 <template>
   <default>
-    <div @touchstart="preventCollapse" class="scrollable-element">
-      <Position />
-      <WaitList />
-    </div>
+    <Position />
+    <WaitList />
   </default>
 </template>
 
 <script setup lang="ts">
-function ensureDocumentIsScrollable() {
-  const isScrollable =
-    document?.documentElement?.scrollHeight > window?.innerHeight;
-  if (!isScrollable) {
-    document?.documentElement?.style?.setProperty(
-      "height",
-      "calc(100vh + 1px)",
-      "important"
-    );
-  }
-}
-ensureDocumentIsScrollable();
-
-function preventCollapse(event) {
-  if (window.scrollY === 0) {
-    window.scrollTo(0, 1);
-  }
-}
+window?.Telegram && window.Telegram.WebApp.disableClosingConfirmation()
 </script>
 
 <style>
