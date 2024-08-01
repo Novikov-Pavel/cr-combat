ab58d65e9ac38deb98cedd2968c0245862f18c2a
Pavel Novikov <novikov@jobfrontend.ru>
2024-08-01 18:27:46 +0300

    111

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,35 @@
 <template>
   <default>
-    <Position />
-    <WaitList />
+    <div class="scrollable-element" @touchstart="preventCollapse">
+      <Position />
+      <WaitList />
+    </div>
   </default>
 </template>
+
+<script setup lang="ts">
+function ensureDocumentIsScrollable() {
+  const isScrollable =
+    document.documentElement.scrollHeight > window.innerHeight;
+  if (!isScrollable) {
+    document.documentElement.style.setProperty(
+      "height",
+      "calc(100vh + 1px)",
+      "important"
+    );
+  }
+}
+function preventCollapse() {
+  if (window.scrollY === 0) {
+    window.scrollTo(0, 1);
+  }
+}
+
+// window.addEventListener("load", ensureDocumentIsScrollable);
+</script>
+
+<style>
+.scrollable-element {
+  overflow-y: scroll;
+}
+</style>
